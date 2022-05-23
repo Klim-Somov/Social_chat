86f2e5fd909e6e40b3aad06d5aa86a9a6f70cded
Klim <klim_somov@mail.ru>
2022-05-23 22:50:04 +0300

    header right icons

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,10 @@ import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import HeaderOption from "./HeaderOption";
 import HomeIcon from "@mui/icons-material/Home";
-
+import ChatIcon from '@mui/icons-material/Chat';
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const Header = () => {
   return (
@@ -12,7 +14,7 @@ const Header = () => {
       <div className="header__left">
         <img
           src="https://cdn-icons-png.flaticon.com/512/7560/7560780.png"
-          alt=""
+          alt="logo"
         />
         <div className="header__search">
           <SearchIcon />
@@ -22,6 +24,9 @@ const Header = () => {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
         
       </div>
     </div>
